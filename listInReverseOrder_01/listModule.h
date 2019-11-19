2437e67877baec997b45dc5fb1dc5d1d8c4831fd
Luca Didaci”git config --global user.email “didaci@gmail.com <didaci@gmail.com>
2019-11-19 10:59:09 +0100

    Initial commit

---
+++ listInReverseOrder_01/listModule.h
@@ -0,0 +1,23 @@
+
+
+#define FALSE 0
+#define TRUE !(FALSE)
+
+typedef struct element {
+  int val;
+  struct element *next;
+} T_element;
+
+typedef int Bool;
+
+Bool isEven(int v);
+
+Bool isEmpty(T_element * p);
+
+T_element * newList(int n);
+
+T_element * newListFromArray(int *arr, int n );
+
+void showList(T_element * p);
+
+void showListReverse(T_element * p);
---
+++ listInReverseOrder_01/readme.md
@@ -0,0 +1,16 @@
+**Linked list**
+
+
+
+* create a linked list with n integer elements
+* display the list
+* show all elements in the list in reverse order, using a recursive approach
+
+
+
+
+
+
+
+
+
